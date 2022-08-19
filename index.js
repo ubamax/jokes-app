e827f1703aab28a266c5d058aab52364ef70f6c5
unknown <peterayuba75@yahoo.com>
2022-08-19 03:00:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,25 +2,24 @@ const image = document.querySelector("img");
 const jokeDIV = document.querySelector("#display-joke");
 const button = document.querySelector("#get-joke");
 
-function getRandomJoke() {
-  const ajax = new XMLHttpRequest();
-  const url = "https://api.chucknorris.io/jokes/random";
-
-  ajax.open("GET", url, true);
-
-  ajax.onreadystatechange = function () {
-    if (this.status === 200 && this.readyState === 4) {
-      console.log(this.responseText);
-      let data = JSON.parse(this.responseText);
-      jokeDIV.innerHTML = `${data.value}`;
-    } else {
-      this.onerror = onerror();
-    }
-  };
-  ajax.send();
-}
-
 button.addEventListener("click", function () {
     getRandomJoke();
   });
   
+  function getRandomJoke() {
+    const ajax = new XMLHttpRequest();
+    const url = "https://api.chucknorris.io/jokes/random";
+  
+    ajax.open("GET", url, true);
+  
+    ajax.onreadystatechange = function () {
+      if (this.status === 200 && this.readyState === 4) {
+        console.log(this.responseText);
+        let data = JSON.parse(this.responseText);
+        jokeDIV.innerHTML = `${data.value}`;
+      } else {
+        this.onerror = onerror();
+      }
+    };
+    ajax.send();
+  }
